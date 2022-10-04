a43462cef53231764bb304afcfb0ca1189ed8ae4
RamshaRauf <113073103+RamshaRauf@users.noreply.github.com>
2022-10-04 15:15:01 -0400

    Runs the entire program

---
+++ Main (4).java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    // Maze maze = new Maze();
+    // MazeViewer viewer = new MazeViewer(maze);
+    
+    // System.out.println(maze.solver(maze.getStart()));
+  }
+}
